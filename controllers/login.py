19439dfa7ac920a507c66c55f509273ed02307bd
Brandon W <bwaggone@umich.edu>
2016-10-30 16:51:46 -0400

    Creating accounts works

--- controllers/login.py
+++ controllers/login.py
@@ -1,6 +1,7 @@
 from flask import *
 from config import app
 login = Blueprint('login', __name__, template_folder = 'views')
+from mysql import ins_usr
 
 @app.route('/signup', methods=['GET'])
 def signup_get():
@@ -11,8 +12,7 @@ def signup_post():
         print request.form['full-name']
         print request.form['password']
         print request.form['confirm-password']
-        cur = app.mysql.connection.cursor()
-        cur.execute("INSERT INTO user (name, password) VALUES ('{0}', '{1}');".format(request.form['full-name'],request.form['password']))
-	app.mysql.connection.commit()
+        
+        ins_usr(app, "INSERT INTO user (name, password) VALUES ('{0}', '{1}');", (request.form['full-name'],request.form['password']))
         
         return render_template('signup.html')
