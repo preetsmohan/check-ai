df8e043fbda0c0d2275a639589838c95cced9381
sartechb <sarthak1657@gmail.com>
2016-10-30 17:20:27 -0400

    python hotfix

--- controllers/login.py
+++ controllers/login.py
@@ -9,9 +9,9 @@ def signup_get():
 
 @app.route('/signup', methods=['POST'])
 def signup_post():
-        print request.form['full-name']
-        print request.form['password']
-        print request.form['confirm-password']
+        print(request.form['full-name'])
+        print(request.form['password'])
+        print(request.form['confirm-password'])
         
         ins_usr(app, "INSERT INTO user (name, password) VALUES ('{0}', '{1}');", (request.form['full-name'],request.form['password']))
         
