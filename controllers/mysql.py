953e59285b6d111a5e5784b0d08c221edc19d73c
Brandon W <bwaggone@umich.edu>
2016-11-02 20:55:18 -0400

    Got login moderately working with sessions

--- controllers/mysql.py
+++ controllers/mysql.py
@@ -1,7 +1,10 @@
-def pref_sql(app, query, parameters):
+from config import app
+
+def pref_sql(query, parameters):
     connection = app.mysql.connection
     cursor = connection.cursor()
     cursor.execute(query.format(*parameters))
+    print(query.format(*parameters))
     connection.commit()
     result = cursor.fetchall()
     return result
