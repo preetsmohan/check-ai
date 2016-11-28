ef95a8d0fb273f23f5ef4ee7daa95b46eee087c5
sartechb <sarthak1657@gmail.com>
2016-11-28 15:02:07 -0500

    if this isn't the tightest button animation you've seen you're a liar

--- static/js/pref.js
+++ static/js/pref.js
@@ -2,11 +2,21 @@ function deletePref(objectId){
     $("#"+objectId).remove();
     $("#"+objectId+"-label").remove();
 }
-
+var moveFab;
 $( document ).ready(function() {
 
     // console.log();
     setRadioSelection($("#explevels").attr('data'));
+    moveFab = false;
+
+    $(window).scroll(function() {
+       if($(window).scrollTop() + $(window).height() == $(document).height()) {
+        hideFab();
+    }else{
+        if(moveFab)
+            showFab();
+    }
+});
 });
 
 function setRadioSelection(expLevel){
@@ -34,4 +44,16 @@ function setRadioSelection(expLevel){
         break;
 
     }
+}
+
+function hideFab(){
+    $('#save-fab').animate({top: "150px"}, 100);
+    moveFab = true;
+    // $('#save-fab').hide();
+}
+
+function showFab(){
+    $('#save-fab').animate({top: "0px"}, 100);
+    moveFab = false;
+    // $('#save-fab').show();
 }
